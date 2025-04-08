904edab3c551f7b63008be73fd885d4705a488f9
Harsha-Puranik <harshapuranik79@gmail.com>
2025-04-08 12:29:31 +0530

    Activity_12_2.py

---
+++ LAB_PROGRAMS/Activity_12_2.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+data = pd.read_csv('4laptops.csv')
+
+
+q1 = data['Screen_size_inches'].quantile(0.25)
+q3 = data['Screen_size_inches'].quantile(0.75)
+iqr = q3 - q1
+
+
+lower_bound = q1 - 1.5 * iqr
+upper_bound = q3 + 1.5 * iqr
+
+
+outliers = data[(data['Screen_size_inches'] < lower_bound) | (data['Screen_size_inches'] > upper_bound)]
+
+
+print("Q1 =", q1)
+print("Q3 =", q3)
+print("IQR =", iqr)
+print("Lower Bound =", lower_bound)
+print("Upper Bound =", upper_bound)
+print("Outliers are:\n", outliers['Screen_size_inches'])
+
+OUTPUT:
+Q1 = 13.3  
+Q3 = 15.6  
+IQR = 2.3  
+Lower Bound = 9.85  
+Upper Bound = 19.05  
+Outliers = [20.0, 8.9]
